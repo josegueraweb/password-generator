9d65a725a9ece58ce139c2fb050ff74d89f47db2
Oseguera, Joshua <joshoseguera09@gmail.com>
2023-11-07 14:24:57 -0800

    Created a series of prompts that users will answer to determine the criteria of their password. The inputs from these prompts were then stored in variables to used elsewhere in the application. Several console.log() tests were performed to ensure the values from the prompts were being properly recorded and stored.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,18 @@
 // Assignment code here
+function generatePassword() {
 
+  var passwordLength = prompt("How long would you like your password to be? \n(Please select a number between 8 - 128 characters.)");
+  var includeLowercase = confirm("Would you like any lowercase letters in your password? \n(Select OK for YES or CANCEL for NO)");
+  var includeUppercase = confirm("Would you like any uppercase letters in your password? \n(Select OK for YES or CANCEL for NO)");
+  var includeNumber = confirm("Would you like any numbers in your password? \n(Select OK for YES or CANCEL for NO)");
+  var includeSpecChar = confirm("Would you like any special characters in your password?\n Special Characters include: !  @  #  $  %  ^  &  *  +  ? \n\n(Select OK for YES or CANCEL for NO)");
+
+  console.log("passwordLength:", passwordLength);
+  console.log("includeLowercase:", includeLowercase);
+  console.log("includeUppercase:", includeUppercase);
+  console.log("includeNumber:", includeNumber);
+  console.log("includeSpecChar:", includeSpecChar);
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
